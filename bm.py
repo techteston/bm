6b99bb3a5772034ffd875bcaa29ae41ef0653e1a
techteston <techteston@gmail.com>
2023-07-19 11:09:33 +0530

    simpler bm

---
+++ bm.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+
+
+# In[ ]:
+
+
+st.set_page_config(page_title='NPI with Bass',page_icon=":tada:",layout="wide")
+
+st.title('NPI Forecasts using Bass Diffusion Model')
+st.write("This model helps forecast the sales for a new product using the Bass Diffusion Model")
+
+st.subheader('Enter the Parameters')
+st.write("These Parameters control the sales per period")
+
+st.subheader('The Sales Profile')
+
+st.subheader('The Sales Data')
+
